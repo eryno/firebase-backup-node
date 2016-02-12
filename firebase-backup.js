1fe7d59e2b36c04e48137ae4e0617434263e1314
Eryn O'Neil <eryn.oneil@gmail.com>
2016-02-12 16:08:26 -0600

    Updated to work with newer versions of node
    Also relies less on hard-coded paths

--- firebase-backup.js
+++ firebase-backup.js
@@ -1,10 +1,8 @@
+#!/usr/bin/env node
+
 // config settings. don't add a trailing backslash for any of file locations
-var nodeLocation = '/usr/bin/nodejs'; // location of the Node.js binary
-var nodeLibrary = '/usr/lib/nodejs'; // location of the Node.js core library files
-var firebaseLibrary = '/usr/lib/node_modules/firebase/lib/firebase-node.js'; // location of the Firebase library for Node.js
-var firebaseTokenLibrary = '/usr/lib/node_modules/firebase-token-generator/lib/FirebaseTokenGenerator.js'); // location for Firebase tokenGen library
-var backupLocation = ''; // where all backup files will be saved
-var firebaseURL = '';
+var backupLocation = '/home/you/firebase-backups'; // where all backup files will be saved
+var firebaseURL = 'https://yoururl.firebaseio.com/';
 var firebaseSecret = '';
 
 // no need to edit below this line
@@ -13,29 +11,30 @@ console.log('Starting backup...');
 
 // since this file will be run by cron, it doesn't get executed in a shell environment
 // and won't know where the relative paths are pointing to. so we use absolute file locations
-var fs = require(nodeLibrary + '/fs.js');
-var Firebase = require(firebaseLibrary);
-var FirebaseTokenGenerator = require(firebaseTokenLibrary);
+var fs = require('file-system');
+var Firebase = require('firebase');
+var FirebaseTokenGenerator = require('firebase-token-generator');
 
 var tokenGen = new FirebaseTokenGenerator(firebaseSecret);
 var token = tokenGen.createToken({}, {admin: true});
 
 var rootRef = new Firebase(firebaseURL);
 
-rootRef.auth(token, function(error) {
+rootRef.authWithCustomToken(token, function(error, authData) {
 	if (error) {
 		console.log(error);
 	} else {
+		console.log('Authentication successful.');
 		rootRef.once('value', function(snapshot) {
-		        // construct a filename based on today's date and the exact time in milliseconds
+		        // construct a filename based on today's date and the H:MM 24-hour time
 		        var now = new Date();
-		        var filename = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getTime() + '.json';
+		        var filename = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '_' + now.getHours() + '-' + now.getMinutes() + '.json';
 		
 		        fs.writeFile(backupLocation + '/' + filename, JSON.stringify(snapshot.exportVal()), function(err) {
 		                if(err) {
 		                        console.log(err);
 		                } else {
-		                        console.log('The backup was saved! ' + filename);
+		                        console.log('The backup was saved! ' + backupLocation + '/' + filename);
 		                }
 				
 				// all done, quit node.js
